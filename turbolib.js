88cdd1a1092671b82dbd85e0e9ca323090ff2239
emirhanyener <emirhan-yener@hotmail.com>
2023-03-04 23:42:20 +0300

    update turbolib: import, implement gameobjects

--- turbolib.js
+++ turbolib.js
@@ -1,6 +1,8 @@
 import { config } from "./Config.js";
-import GameObject from "./GameObject.js";
-import GameObjects from "./GameObjects.js";
+import { GameObject } from "./GameObject.js";
+import { Position } from "./Position.js";
+import { Size } from "./Size.js";
+import { GameObjects } from "./GameObjects.js";
 
 let canvas = document.getElementById(config.canvas.id);
 let ctx = canvas.getContext("2d");
@@ -8,10 +10,14 @@ let ctx = canvas.getContext("2d");
 canvas.width = config.canvas.width;
 canvas.height = config.canvas.height;
 
+let gameObjects = new GameObjects();
+
 setInterval(() => {
     run();
 }, 10);
 
 function run(){
-
+    gameObjects.getGameObjects().forEach(element => {
+        console.log(element.getName());
+    });
 }
