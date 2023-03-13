ce1867f4832cfd011c2523905533dddbee127a37
emirhanyener <emirhan-yener@hotmail.com>
2023-03-13 23:09:42 +0300

    physics: add calculate method

--- Physics.js
+++ Physics.js
@@ -4,4 +4,8 @@ export class Physics{
         this.velocity = new Velocity(0, 0);
         this.mass = 0;
     }
+
+    calculate(){
+        this.gameObject.position.add(this.velocity.x, this.velocity.y);
+    }
 }
