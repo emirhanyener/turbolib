aff71e4f65c29c74865bf39b00e1ba58354c263f
emirhanyener <emirhan-yener@hotmail.com>
2023-03-14 22:32:25 +0300

    turbolib: import physics

--- turbolib.js
+++ turbolib.js
@@ -2,6 +2,7 @@ import { config } from "./Config.js";
 import { GameObject } from "./GameObject.js";
 import { Position } from "./Position.js";
 import { Size } from "./Size.js";
+import { Physics } from "Physics.js";
 import { GameObjects } from "./GameObjects.js";
 
 let canvas = document.getElementById(config.canvas.id);
@@ -11,6 +12,7 @@ canvas.width = config.canvas.width;
 canvas.height = config.canvas.height;
 
 export let gameObjects = new GameObjects();
+let physics = [];
 
 setInterval(() => {
     run();
