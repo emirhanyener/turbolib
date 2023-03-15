489fac6345b04d08ecf6f804a2d699cf09fa281f
emirhanyener <emirhan-yener@hotmail.com>
2023-03-15 20:06:05 +0300

    turbolib: fill background

--- turbolib.js
+++ turbolib.js
@@ -2,7 +2,7 @@ import { config } from "./Config.js";
 import { GameObject } from "./GameObject.js";
 import { Position } from "./Position.js";
 import { Size } from "./Size.js";
-import { Physics } from "Physics.js";
+import { Physics } from "./Physics.js";
 import { GameObjects } from "./GameObjects.js";
 
 let canvas = document.getElementById(config.canvas.id);
@@ -12,13 +12,16 @@ canvas.width = config.canvas.width;
 canvas.height = config.canvas.height;
 
 export let gameObjects = new GameObjects();
-let physics = [];
+export let physics = [];
 
 setInterval(() => {
     run();
 }, 10);
 
 function run(){
+    ctx.fillStyle = config.canvas.background_color;
+    ctx.fillRect(0, 0, config.canvas.width, config.canvas.height);
+    ctx.fillStyle = "#000000";
     gameObjects.getGameObjects().forEach(element => {
         ctx.fillRect(element.position.x, element.position.y, element.size.x, element.size.y);
     });
