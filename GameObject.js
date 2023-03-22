e5f730df8235207cf57fdddab1095c85cf588096
emirhanyener <emirhan-yener@hotmail.com>
2023-03-22 20:48:45 +0300

    gameobject: add checktrigger method

--- GameObject.js
+++ GameObject.js
@@ -43,4 +43,27 @@ export class GameObject{
     destroy(){
         gameObjects.getGameObjects().splice(gameObjects.getGameObjects().indexOf(this), 1);
     }
+
+    checkTrigger(offsetX, offsetY, x, y) {
+		let detectedObjects = [];
+		let step = Math.round(Math.sqrt(Math.pow(x - offsetX, 2) + Math.pow(y - offsetY, 2)));
+
+		for (let i = 0; i < step + 2; i++) {
+			gameObjects.getGameObjects().forEach((item) => {
+				if (item == this) {
+					return;
+				}
+				if (this.position.getX() + offsetX + (x / step) * i > item.position.getX() - item.size.getWidth() / 2) {
+					if (this.position.getX() + offsetX + (x / step) * i < item.position.getX() + item.size.getWidth() / 2) {
+						if (this.position.getY() + offsetY + (y / step) * i > item.position.getY() - item.size.getHeight() / 2) {
+							if (this.position.getY() + offsetY + (y / step) * i < item.position.getY() + item.size.getHeight() / 2) {
+								detectedObjects.push(item);
+							}
+						}
+					}
+				}
+			});
+		}
+		return detectedObjects;
+	}
 }
