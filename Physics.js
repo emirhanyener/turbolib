35667f702f432da4160c8cdf31518abd0d0977be
emirhanyener <emirhan-yener@hotmail.com>
2023-03-23 22:00:01 +0300

    physics: add isGrounded

--- Physics.js
+++ Physics.js
@@ -7,13 +7,16 @@ export class Physics{
         this.gameObject = gameObject;
         this.velocity = new Velocity(0, 0);
         this.mass = 1;
+        this.isGrounded = false;
     }
 
     calculate(){
         this.gameObject.position.add(this.velocity.x, this.velocity.y);
         if(this.gameObject.checkTrigger(0, 0, 0, this.gameObject.size.y / 2).length == 0){
+            this.isGrounded = false;
             this.velocity.add(0, this.mass * config.gravity / 100);
         } else {
+            this.isGrounded = true;
             this.velocity.y = 0;
         }
         
