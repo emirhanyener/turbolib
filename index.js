601afc7bd94fc8ca937c0034ae75c918bd13b76a
emirhanyener <emirhan-yener@hotmail.com>
2023-03-25 00:45:23 +0300

    add index js

---
+++ index.js
@@ -0,0 +1,7 @@
+export { config } from "./Config.js";
+export { GameObject } from "./GameObject.js";
+export { Position } from "./Position.js";
+export { Size } from "./Size.js";
+export { Physics } from "./Physics.js";
+export { Resources } from "./Resources.js";
+export { ImageObject } from "./ImageObject.js";
