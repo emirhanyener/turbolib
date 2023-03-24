d6636a8b59324ebdb0fdb342d8cd4944673c37a2
emirhanyener <emirhan-yener@hotmail.com>
2023-03-25 00:43:43 +0300

    imageobject: add animations

--- ImageObject.js
+++ ImageObject.js
@@ -1,7 +1,29 @@
+import { resources } from "./turbolib.js";
+
 export class ImageObject{
     constructor(src, name){
-        this.image = new Image();
-        this.image.src = src;
+        this.image = [];
+        let temp = new Image();
+        temp.src = src;
+        this.image.push(temp);
         this.name = name;
+        this.index = 0;
+        this.time = 0;
+        this.animationSpeed = 10;
+    }
+
+    setAnimationSpeed(newSpeed){
+        this.animationSpeed = newSpeed;
+    }
+    addImage(name){
+        this.image.push(resources.findImage(name).image[0]);
+    }
+    getImage(){
+        if(this.time > this.animationSpeed){
+            this.index++;
+            this.time = 0;
+        }
+        this.time++;
+        return this.image[this.index % this.image.length];
     }
 }
