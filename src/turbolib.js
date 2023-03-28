ca85909d5c1eccb9cefaebd01567d87defd70266
emirhanyener <emirhan-yener@hotmail.com>
2023-03-29 01:51:04 +0300

    turbolib: draw image flip

--- src/turbolib.js
+++ src/turbolib.js
@@ -23,7 +23,13 @@ async function getImages(){
 
 let fetch_data = await getImages();
 fetch_data.images.forEach((item) => resources.addImage(new ImageObject(item.src, item.name)));
-fetch_data.animations.forEach((item) => resources.createAnimation(item.name, item.images, item.speed));
+fetch_data.animations.forEach((item) => {
+    resources.createAnimation(item.name, item.images, item.speed);
+    if(item.flipVertical)
+        resources.findImage(item.name).changeFlipVertical();
+    if(item.flipHorizontal)
+        resources.findImage(item.name).changeFlipHorizontal();
+});
 
 setInterval(() => {
     run();
@@ -35,9 +41,11 @@ function run(){
     ctx.fillRect(0, 0, config.canvas.width, config.canvas.height);
     resources.getGameObjects().forEach(element => {
         ctx.fillStyle = "#000000";
-        if(element.image)
-            ctx.drawImage(element.image.getImage(), element.position.x - element.size.x / 2, element.position.y - element.size.y / 2, element.size.x, element.size.y) 
-        else{
+        if(element.image){
+            ctx.scale(1 * element.image.getFlipHorizontal(), 1 * element.image.getFlipVertical());
+            ctx.drawImage(element.image.getImage(), element.image.getFlipHorizontal() * (element.position.x - element.size.x / 2 + (element.image.flipHorizontal ? element.size.x : 0)), element.image.getFlipVertical() * (element.position.y - element.size.y / 2 + (element.image.flipVertical ? element.size.y : 0)), element.size.x, element.size.y);
+            ctx.scale(1 * element.image.getFlipHorizontal(), 1 * element.image.getFlipVertical());
+        }else{
             if(element.color)
                 ctx.fillStyle = element.color;
                 ctx.fillRect(element.position.x - element.size.x / 2, element.position.y - element.size.y / 2, element.size.x, element.size.y);
