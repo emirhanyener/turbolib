db12b3152b7c46a8525b91f8f074d2cad4dc8ef2
emirhanyener <emirhan-yener@hotmail.com>
2023-04-03 00:44:48 +0300

    followerCamera: add delay time to camera position

--- src/FollowerCamera.js
+++ src/FollowerCamera.js
@@ -6,9 +6,13 @@ export class FollowerCamera extends Camera{
     constructor(gameobject){
         super(0, 0);
         this.gameobject = gameobject;
+        this.delay = 1;
     }
     getPosition(){
-        this.position.update(this.gameobject.position.x - config.canvas.width / 2, this.gameobject.position.y - config.canvas.height / 2);
+        this.position.update(this.position.x + (this.gameobject.position.x - this.position.x) * this.delay - (config.canvas.width / 2) * this.delay, this.gameobject.position.y - config.canvas.height / 2);
         return this.position;
+    }
+    setDelay(delay){
+        this.delay = delay;
     }
 }
