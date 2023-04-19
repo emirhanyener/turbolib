b051a1193b6310605dc49562219c30a87a08e945
emirhanyener <emirhan-yener@hotmail.com>
2023-04-20 02:21:20 +0300

    add UI class

---
+++ src/UI/UI.js
@@ -0,0 +1,12 @@
+import { UIType } from "./UIType.js";
+
+export class UI {
+    constructor(type, position) {
+        this.type = type;
+        this.position = position;
+    }
+
+    drawUI(){
+
+    }
+}
