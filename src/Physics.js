7e5096721f8c0b2fe6c4c0e660e9ee1b262eb5df
emirhanyener <emirhan-yener@hotmail.com>
2023-04-25 00:20:12 +0300

    physics: draw velocity line

--- src/Physics.js
+++ src/Physics.js
@@ -1,5 +1,8 @@
 import { config } from "./Config.js";
+import { UILine } from "./UI/UILine.js";
+import { UIType } from "./UI/UIType.js";
 import { Vector } from "./Vector.js";
+import { resources } from "./turbolib.js";
 
 export class Physics {
   constructor(gameObject) {
@@ -8,15 +11,29 @@ export class Physics {
     this.mass = 1;
     this.isGrounded = false;
     this.bounceRate = 0;
+    this.isVisible = false;
+  }
+
+  visible(){
+    resources.addUI(new UILine(UIType.Line, "downLine1", new Vector(0, 0)));
+    resources.addUI(new UILine(UIType.Line, "downLine2", new Vector(0, 0)));
+    resources.addUI(new UILine(UIType.Line, "upLine1", new Vector(0, 0)));
+    resources.addUI(new UILine(UIType.Line, "upLine2", new Vector(0, 0)));
+    this.isVisible = !this.isVisible;
   }
 
   calculate() {
     this.gameObject.position.add(this.velocity.x, this.velocity.y);
-
+    if(this.isVisible){
+      resources.findUI("downLine1").updateFromTo(this.gameObject.position.x - this.gameObject.size.x / 2, this.gameObject.position.y + this.gameObject.size.y / 2, this.gameObject.position.x + this.gameObject.size.x / 2, this.gameObject.position.y + this.gameObject.size.y / 2 + Math.round(this.velocity.y < 0 ? 0 : this.velocity.y));
+      resources.findUI("downLine2").updateFromTo(this.gameObject.position.x + this.gameObject.size.x / 2, this.gameObject.position.y + this.gameObject.size.y / 2, this.gameObject.position.x - this.gameObject.size.x / 2, this.gameObject.position.y + this.gameObject.size.y / 2 + Math.round(this.velocity.y < 0 ? 0 : this.velocity.y));
+      resources.findUI("upLine1").updateFromTo(this.gameObject.position.x - this.gameObject.size.x / 2, this.gameObject.position.y - this.gameObject.size.y / 2, this.gameObject.position.x + this.gameObject.size.x / 2, this.gameObject.position.y - this.gameObject.size.y / 2 + Math.round(this.velocity.y > 0 ? 0 : this.velocity.y));
+      resources.findUI("upLine2").updateFromTo(this.gameObject.position.x + this.gameObject.size.x / 2, this.gameObject.position.y - this.gameObject.size.y / 2, this.gameObject.position.x - this.gameObject.size.x / 2, this.gameObject.position.y - this.gameObject.size.y / 2 + Math.round(this.velocity.y > 0 ? 0 : this.velocity.y));
+    }
     //down
     let downArray = this.gameObject
       .checkTrigger(
-        -this.gameObject.size.x / 2,
+        - this.gameObject.size.x / 2,
         this.gameObject.size.y / 2,
         this.gameObject.size.x,
         Math.round(this.velocity.y < 0 ? 0 : this.velocity.y)
