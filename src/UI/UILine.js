4d48f8b0049c2be3276d92d38c4e6024e8b5b2a8
emirhanyener <emirhan-yener@hotmail.com>
2023-04-26 00:11:30 +0300

    UILine: gameworld condition

--- src/UI/UILine.js
+++ src/UI/UILine.js
@@ -3,8 +3,8 @@ import { resources } from "../turbolib.js";
 import { UI } from "./UI.js";
 
 export class UILine extends UI{
-    constructor(type, name, position){
-        super(type, name, position);
+    constructor(name, position){
+        super(name, position);
         this.toPosition = new Vector(0, 0);
     }
 
@@ -16,8 +16,13 @@ export class UILine extends UI{
     drawUI(context){
         context.strokeStyle = "#FF0000";
         context.beginPath();
-        context.moveTo(this.position.x - resources.getMainCamera().getPosition().x, this.position.y - resources.getMainCamera().getPosition().y);
-        context.lineTo(this.toPosition.x - resources.getMainCamera().getPosition().x, this.toPosition.y - resources.getMainCamera().getPosition().y);
+        if(this.gameWorld){
+            context.moveTo(this.position.x - resources.getMainCamera().getPosition().x, this.position.y - resources.getMainCamera().getPosition().y);
+            context.lineTo(this.toPosition.x - resources.getMainCamera().getPosition().x, this.toPosition.y - resources.getMainCamera().getPosition().y);
+        } else {
+            context.moveTo(this.position.x, this.position.y);
+            context.lineTo(this.toPosition.x, this.toPosition.y);
+        }
         context.stroke();
     }
 }
