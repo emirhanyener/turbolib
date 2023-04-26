17dbbeee23a303c19fe7a2c4152091b4fbee0086
emirhanyener <emirhan-yener@hotmail.com>
2023-04-26 16:07:49 +0300

    add uibox class

---
+++ src/UI/UIBox.js
@@ -0,0 +1,25 @@
+import { Vector } from "../Vector.js";
+import { resources } from "../turbolib.js";
+import { UI } from "./UI.js";
+
+export class UIBox extends UI{
+    constructor(name, position, size){
+        super(name, position);
+        this.size = size;
+    }
+
+    updateSize(sizeX, sizeY){
+        this.size.update(sizeX, sizeY);
+    }
+
+    drawUI(context){
+        super.drawUI(context);
+        context.beginPath();
+        if(this.gameWorld){
+            context.fillRect(this.position.x - resources.getMainCamera().getPosition().x, this.position.y - resources.getMainCamera().getPosition().y, this.size.x, this.size.y);
+        } else {
+            context.fillRect(this.position.x, this.position.y, this.size.x, this.size.y);
+        }
+        context.stroke();
+    }
+}
