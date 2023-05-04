f95c26035b74141dc1960fe949b8aa58b8401f01
emirhanyener <emirhan-yener@hotmail.com>
2023-05-05 00:05:24 +0300

    update object function

--- src/ObjectFunction.js
+++ src/ObjectFunction.js
@@ -1,9 +1,8 @@
-export class ObjectFunction{
+import { Function } from "./Function.js";
+
+export class ObjectFunction extends Function{
     constructor(gameobject){
+        super();
         this.gameobject = gameobject;
-    }
-
-    run(){
-
     }
 }
