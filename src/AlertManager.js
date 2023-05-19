75d20032b2e53548cf18bd463b293df367fa71f9
emirhanyener <emirhan-yener@hotmail.com>
2023-05-19 23:56:20 +0300

    alertmanager: add alert method

--- src/AlertManager.js
+++ src/AlertManager.js
@@ -1,6 +1,12 @@
+import { Alert } from "./index.js";
+
 export class AlertManager {
     constructor() {
         this.alerts = [];
         this.delayTime;
+    }
+
+    addAlert(text, time){
+        this.alerts.push(new Alert(text, time));
     }
 }
