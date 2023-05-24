b2da41770146de141040dfbdf841c1827b9e287c
emirhanyener <emirhan-yener@hotmail.com>
2023-05-24 23:25:28 +0300

    update alert manager

--- src/AlertManager.js
+++ src/AlertManager.js
@@ -3,11 +3,15 @@ import { Alert } from "./index.js";
 export class AlertManager {
     constructor() {
         this.alerts = [];
+        this.maxAlert = 3;
         this.delayTime;
     }
 
-    addAlert(text, time){
-        this.alerts.push(new Alert(text, time));
+    addAlert(text, time, color){
+        if(this.alerts.length > this.maxAlert){
+            this.removeFirstAlert();
+        }
+        this.alerts.push(new Alert(text, time, color));
     }
 
     removeFirstAlert(){
