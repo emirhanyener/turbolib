4c44d8cb81280b668ae8d6724b1f42f5c449a502
emirhanyener <emirhan-yener@hotmail.com>
2023-06-17 23:13:23 +0300

    vector: comments

--- src/Vector.js
+++ src/Vector.js
@@ -4,16 +4,30 @@ export class Vector{
         this.y = y;
     }
 
+    /**
+     * Set new vector param.
+     * @param {int} x 
+     * @param {int} y 
+     */
     update(x, y){
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Sum current vector and param.
+     * @param {int} x 
+     * @param {int} y 
+     */
     add(x, y){
         this.x += x;
         this.y += y;
     }
 
+    /**
+     * Multiply current vector with scale param.
+     * @param {int} scale 
+     */
     scale(scale){
         this.x = x * scale;
         this.y = y * scale;
