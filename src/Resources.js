84b6fa146b9376d7b8e4dc92f9dbf11ea1e5b232
emirhanyener <emirhan-yener@hotmail.com>
2023-09-24 11:57:20 +0300

    resources: implement scene

--- src/Resources.js
+++ src/Resources.js
@@ -1,20 +1,15 @@
 import {
-  Camera,
-  GameObject,
-  Physics,
   ImageObject,
   Vector,
   AlertManager,
-  UI,
+  Scene
 } from "./index.js";
 
 export class Resources {
   constructor() {
-    this.gameobjects = [];
-    this.ui = [];
+    this.scene = new Scene();
     this.images = [];
     this.alertManager = new AlertManager();
-    this.camera = new Camera(0, 0);
     this.mouse = {
       worldPosition: new Vector(0, 0),
       canvasPosition: new Vector(0, 0),
@@ -22,21 +17,6 @@ export class Resources {
     };
     this._mouse = new Vector(0, 0);
     this.gameTime = 0;
-  }
-
-  /**
-   * Get current camera.
-   * @returns Camera
-   */
-  getMainCamera() {
-    return this.camera;
-  }
-
-  /**
-   * Set new main camera.
-   */
-  setMainCamera(camera) {
-    this.camera = camera;
   }
 
   /**
@@ -49,30 +29,6 @@ export class Resources {
       this.mouse.canvasPosition.y + this.camera.position.y
     );
     return this.mouse.worldPosition;
-  }
-
-  /**
-   * Get all added gameobject elements.
-   */
-  getGameObjects() {
-    return this.gameobjects;
-  }
-
-  /**
-   *
-   * @param {GameObject} gameObject
-   */
-  addGameObject(gameObject) {
-    if (this.isGameObjectExists(gameObject.name)) {
-      console.error("GameObject already exists");
-      return;
-    }
-    this.gameobjects.push(gameObject);
-  }
-  isGameObjectExists(name) {
-    if (this.gameobjects.find((gameObject) => gameObject.name == name))
-      return true;
-    return false;
   }
 
   /**
@@ -97,29 +53,6 @@ export class Resources {
    */
   addAlert(text, color) {
     this.alertManager.addAlert(text, this.gameTime, color);
-  }
-
-  /**
-   * Get all UI elements.
-   */
-  getUI() {
-    return this.ui;
-  }
-
-  /**
-   * Add new UI element.
-   * @param {UI} item
-   */
-  addUI(item) {
-    if (this.isUIExists(item.name)) {
-      console.error("UI already exists");
-      return;
-    }
-    this.ui.push(item);
-  }
-  isUIExists(name) {
-    if (this.images.find((image) => image.name == name)) return true;
-    return false;
   }
 
   /**
