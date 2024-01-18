e24660627ae3b5623aa48979032cf2e0faea7f26
emirhanyener <emirhan-yener@hotmail.com>
2024-01-18 12:42:35 +0300

    config: add debug

--- src/Config.js
+++ src/Config.js
@@ -1,12 +1,13 @@
 export let config = {
   canvas: {
     id: "canvas",
-    width: 400,
-    height: 400,
+    width: window.innerWidth,
+    height: window.innerHeight,
     resolution_multipler: 1,
     background: "",
     background_color: "#FFFFFF",
   },
+  debug: true,
   path: "",
   gravity: 9.81,
 };
