b68c87721bb8293a77a85f26437816ffe1540755
emirhanyener <emirhan-yener@hotmail.com>
2024-01-21 11:09:14 +0300

    physics: gameobject activity

--- src/Utils/Physics.js
+++ src/Utils/Physics.js
@@ -12,104 +12,106 @@ export class Physics {
   }
 
   calculate() {
-    //down
-    let downArray = this.gameObject
-      .checkTrigger(
-        -this.gameObject.size.x / 2,
-        this.gameObject.size.y / 2,
-        this.gameObject.size.x,
-        Math.round(this.velocity.y < 0 ? 0 : this.velocity.y)
-      )
-      .concat(
-        this.gameObject.checkTrigger(
-          this.gameObject.size.x / 2,
+    if (this.gameObject.active) {
+      //down
+      let downArray = this.gameObject
+        .checkTrigger(
+          -this.gameObject.size.x / 2,
           this.gameObject.size.y / 2,
-          -this.gameObject.size.x,
+          this.gameObject.size.x,
           Math.round(this.velocity.y < 0 ? 0 : this.velocity.y)
         )
-      );
-    if (downArray.length == 0) {
-      this.isGrounded = false;
-      if (this.isGravityActive) {
-        this.velocity.add(0, (this.mass * config.gravity) / 100);
+        .concat(
+          this.gameObject.checkTrigger(
+            this.gameObject.size.x / 2,
+            this.gameObject.size.y / 2,
+            -this.gameObject.size.x,
+            Math.round(this.velocity.y < 0 ? 0 : this.velocity.y)
+          )
+        );
+      if (downArray.length == 0) {
+        this.isGrounded = false;
+        if (this.isGravityActive) {
+          this.velocity.add(0, (this.mass * config.gravity) / 100);
+        }
       }
-    }
-    if (downArray.length > 0) {
-      this.isGrounded = true;
-      this.velocity.y = -this.velocity.y * this.bounceRate;
-      this.gameObject.position.y =
-        downArray[0].position.y -
-        downArray[0].size.y / 2 -
-        this.gameObject.size.y / 2;
-    }
+      if (downArray.length > 0) {
+        this.isGrounded = true;
+        this.velocity.y = -this.velocity.y * this.bounceRate;
+        this.gameObject.position.y =
+          downArray[0].position.y -
+          downArray[0].size.y / 2 -
+          this.gameObject.size.y / 2;
+      }
 
-    //up
-    let upArray = this.gameObject
-      .checkTrigger(
-        -this.gameObject.size.x / 2,
-        -this.gameObject.size.y / 2,
-        this.gameObject.size.x,
-        Math.round(this.velocity.y > 0 ? 0 : this.velocity.y)
-      )
-      .concat(
-        this.gameObject.checkTrigger(
-          this.gameObject.size.x / 2,
+      //up
+      let upArray = this.gameObject
+        .checkTrigger(
+          -this.gameObject.size.x / 2,
           -this.gameObject.size.y / 2,
-          -this.gameObject.size.x,
+          this.gameObject.size.x,
           Math.round(this.velocity.y > 0 ? 0 : this.velocity.y)
         )
-      );
-    if (upArray.length > 0) {
+        .concat(
+          this.gameObject.checkTrigger(
+            this.gameObject.size.x / 2,
+            -this.gameObject.size.y / 2,
+            -this.gameObject.size.x,
+            Math.round(this.velocity.y > 0 ? 0 : this.velocity.y)
+          )
+        );
       if (upArray.length > 0) {
-        this.gameObject.position.y =
-          upArray[0].position.y +
-          upArray[0].size.y / 2 +
-          this.gameObject.size.y / 2 +
-          1;
+        if (upArray.length > 0) {
+          this.gameObject.position.y =
+            upArray[0].position.y +
+            upArray[0].size.y / 2 +
+            this.gameObject.size.y / 2 +
+            1;
+        }
+        this.velocity.y = -this.velocity.y * this.bounceRate;
       }
-      this.velocity.y = -this.velocity.y * this.bounceRate;
-    }
 
-    //right
-    let rightArray = this.gameObject
-      .checkTrigger(
-        this.gameObject.size.x / 2,
-        this.gameObject.size.y / 2 - 1,
-        Math.round(this.velocity.x < 0 ? 0 : this.velocity.x),
-        -this.gameObject.size.y + 2
-      )
-      .concat(
-        this.gameObject.checkTrigger(
+      //right
+      let rightArray = this.gameObject
+        .checkTrigger(
           this.gameObject.size.x / 2,
-          -this.gameObject.size.y / 2 + 1,
+          this.gameObject.size.y / 2 - 1,
           Math.round(this.velocity.x < 0 ? 0 : this.velocity.x),
-          this.gameObject.size.y - 2
+          -this.gameObject.size.y + 2
         )
-      );
-    if (rightArray.length > 0) {
-      this.velocity.x = 0;
-    }
+        .concat(
+          this.gameObject.checkTrigger(
+            this.gameObject.size.x / 2,
+            -this.gameObject.size.y / 2 + 1,
+            Math.round(this.velocity.x < 0 ? 0 : this.velocity.x),
+            this.gameObject.size.y - 2
+          )
+        );
+      if (rightArray.length > 0) {
+        this.velocity.x = 0;
+      }
 
-    //left
-    let leftArray = this.gameObject
-      .checkTrigger(
-        -this.gameObject.size.x / 2,
-        this.gameObject.size.y / 2 - 1,
-        Math.round(this.velocity.x > 0 ? 0 : this.velocity.x),
-        -this.gameObject.size.y + 2
-      )
-      .concat(
-        this.gameObject.checkTrigger(
+      //left
+      let leftArray = this.gameObject
+        .checkTrigger(
           -this.gameObject.size.x / 2,
-          -this.gameObject.size.y / 2 + 1,
+          this.gameObject.size.y / 2 - 1,
           Math.round(this.velocity.x > 0 ? 0 : this.velocity.x),
-          this.gameObject.size.y - 2
+          -this.gameObject.size.y + 2
         )
-      );
-    if (leftArray.length > 0) {
-      this.velocity.x = 0;
+        .concat(
+          this.gameObject.checkTrigger(
+            -this.gameObject.size.x / 2,
+            -this.gameObject.size.y / 2 + 1,
+            Math.round(this.velocity.x > 0 ? 0 : this.velocity.x),
+            this.gameObject.size.y - 2
+          )
+        );
+      if (leftArray.length > 0) {
+        this.velocity.x = 0;
+      }
+      this.gameObject.position.add(this.velocity.x, this.velocity.y);
     }
-    this.gameObject.position.add(this.velocity.x, this.velocity.y);
   }
 
   getMass() {
